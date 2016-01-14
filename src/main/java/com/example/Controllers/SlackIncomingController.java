c582f26f9a98114716fb31fc86012e71bd920981
Bill Ramsey <billramsey@gmail.com>
2016-01-13 16:49:24 -0800

    attempt at springify

---
+++ src/main/java/com/example/Controllers/SlackIncomingController.java
@@ -0,0 +1,53 @@
+package com.example.Controllers;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+import java.util.List;
+
+@RestController
+public class SlackIncomingController {
+  //Tower
+  //Board
+  
+  @RequestMapping("/")
+  public String index() {
+      return "Greetings from Spring Boot!";
+  }
+/*
+  @RequestMapping(value="/", method=RequestMethod.POST, 
+        consumes = {"application/x-www-form-urlencoded"})
+  public @ResponseBody String getSlackRequest(@ModelAttribute SlackRequest s) {
+    System.out.println(s.toString());
+
+    String text = s.getText();
+    Arguments args = new Arguments(text);
+    Command command = commandService.findCommand(args.getCommand());
+    
+    if (command == null) {
+      String invalidText = 
+          messageByLocaleService.getMessage("command.not.exist", args.getCommand());
+      return new SlackResponse(invalidText).toJSONString();
+    }
+
+    if (!command.areArgsGood(args)) {   
+      String invalidText = 
+          messageByLocaleService.getMessage("command.invalid", command.usage());
+      return new SlackResponse(invalidText).toJSONString();
+    }
+    
+    
+    SlackResponse slackResponse = command.execute(s, args);
+
+    return slackResponse.toJSONString();
+  }
+  */
+
+}
---
+++ src/main/java/com/example/SlackApplication.java
@@ -0,0 +1,60 @@
+package com.example;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class SlackApplication extends SpringBootServletInitializer {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SlackApplication.class, args);
+  }
+  
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(SlackApplication.class);
+  }
+
+  @Bean
+  public LocaleResolver localeResolver() {
+    SessionLocaleResolver slr = new SessionLocaleResolver();
+    slr.setDefaultLocale(Locale.US);
+    return slr;
+  }
+  
+  @Bean
+  public ReloadableResourceBundleMessageSource messageSource() {
+    ReloadableResourceBundleMessageSource messageSource = 
+        new ReloadableResourceBundleMessageSource();
+    messageSource.setBasename("classpath:locale/messages");
+    messageSource.setCacheSeconds(3600); //refresh cache once per hour
+    return messageSource;
+  }
+  /*
+  @Bean
+  public DatabaseService databaseService() {
+    return new MongoDatabaseService();
+  }
+
+  @Bean
+  public SlackService slackService() {
+    return new SlackServiceImpl();
+  }
+  */
+
+}
